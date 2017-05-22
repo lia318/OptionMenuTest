4452dc67e1d59fe655d45ed5c057532e632df05d
sojung kim <sojung kim>
2017-05-22 20:52:30 +0900

    #2 onCreateOptionsMenu Overriding

--- app/src/main/java/kr/hs/lia318e_mirim/optionmenutest/MainActivity.java
+++ app/src/main/java/kr/hs/lia318e_mirim/optionmenutest/MainActivity.java
@@ -2,6 +2,8 @@ package kr.hs.lia318e_mirim.optionmenutest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu1,menu); // 여기서 menu는 매개변수의 menu이다.
+        return true;
     }
 }
